b3377a4eb4b945f9a3bd045f7651c5f4234fe56f
mayur lavadiya <mayurlavadiya123@gmail.com>
2023-01-02 20:13:01 +0530

    inser/update

---
+++ 3-api-insert.php
@@ -0,0 +1,25 @@
+<?php
+
+include "config.php";
+
+header('content-type: application/json');
+header('Access-Control-Allow-origin: *'); // easily access krva mate phn n web ma
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-type,Access-Control-Allow-Methods,Authorization,X-Requested-with');
+
+$data = json_decode(file_get_contents("php://input"), true); // json decode array ma convert krva mate
+
+$firstname = $data['firstname'];
+$lastname = $data['lastname'];
+
+$sql = "INSERT INTO stud_detail(id,firstname,lastname) VALUES ('','$firstname','$lastname')";
+
+if(mysqli_query($conn, $sql))
+{
+    echo json_encode(array('message'=>'Student record inserted', 'status'=> true)); // array bnavyo n 2 key lidhi msg n status n ene value api
+}
+else{
+    echo json_encode(array('message'=>'Student record not inserted', 'status'=> false)); // array bnavyo n 2 key lidhi msg n status n ene value api
+}
+
+?>
---
+++ 4-api-update.php
@@ -0,0 +1,25 @@
+<?php
+
+include "config.php";
+
+header('content-type: application/json');
+header('Access-Control-Allow-origin: *'); // easily access krva mate phn n web ma
+header('Access-Control-Allow-Methods: PUT'); // PUT for update
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-type,Access-Control-Allow-Methods,Authorization,X-Requested-with');
+
+$data = json_decode(file_get_contents("php://input"), true); // json decode array ma convert krva mate
+$student_id = $data['id'];
+$firstname = $data['firstname'];
+$lastname = $data['lastname'];
+
+$sql = "UPDATE stud_detail SET firstname = '$firstname', lastname = '$lastname' WHERE id = '$student_id'";
+
+if(mysqli_query($conn, $sql))
+{
+    echo json_encode(array('message'=>'Student record updated', 'status'=> true)); // array bnavyo n 2 key lidhi msg n status n ene value api
+}
+else{
+    echo json_encode(array('message'=>'Student record not updated', 'status'=> false)); // array bnavyo n 2 key lidhi msg n status n ene value api
+}
+
+?>
