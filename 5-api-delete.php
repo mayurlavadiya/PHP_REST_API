62e45e19bb30104b6c8f15842cb1a4c64836b6b2
mayur lavadiya <mayurlavadiya123@gmail.com>
2023-01-03 18:46:56 +0530

    Delete

---
+++ 5-api-delete.php
@@ -0,0 +1,21 @@
+<?php
+include "config.php";
+
+header('content-type: application/json');
+header('Access-control-Allow-origin: *'); // easily access krva mate phn n web ma
+header('Access-Control-Allow-Methods: DELETE'); // PUT for update
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-type,Access-Control-Allow-Methods,Authorization,X-Requested-with');
+
+$data = json_decode(file_get_contents("php://input"), true); // json decode array ma convert krva mate
+
+$student_id = $data['id'];
+
+$sql = "DELETE FROM stud_detail WHERE id = $student_id";
+
+if(mysqli_query($conn, $sql)){
+    echo json_encode(array('message'=>'Record Deleted Successfully...', 'status'=> true)); // array bnavyo n 2 key lidhi msg n status n ene value api
+}else{
+    echo json_encode(array('message'=>'Unable to delete record...!', 'status'=> false)); // array bnavyo n 2 key lidhi msg n status n ene value api
+}
+
+?>
